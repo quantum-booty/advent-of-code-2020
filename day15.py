94f401350f3d6a96f0a8f28b9f37d8a77a36f2f3
henry wang <henrywang97@gmail.com>
2020-12-15 18:46:38 +0000

    Refactored to use iterator instead of a for loop.

--- day15.py
+++ day15.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-from typing import DefaultDict, List
+from typing import DefaultDict, Iterator, List
+import itertools
 
 # if the previous number is not called before, then the next number will be 0.
 # The next number will be the difference in turns of when the previous number was last called.
@@ -7,15 +8,16 @@ from typing import DefaultDict, List
 # Use a dictionary to model this, use number as key, position as value.
 
 
-def part1(input: List[int], turn_goal: int = 2020):
+def play_game(starting_nums: List[int]) -> Iterator[int]:
+    # Trying out the concept of generator/iterator from Joel Grus.
     last_seen: DefaultDict[int, int] = defaultdict(int)
-    for turn, called in enumerate(input):
+    for turn, called in enumerate(starting_nums):
         last_seen[called] = turn + 1
 
-    turn = len(input) + 1
+    turn = len(starting_nums) + 1
     prev_called = 0
     # all_called = input + [0]
-    while True:
+    for turn in itertools.count(len(starting_nums) + 1):
         if last_seen[prev_called]:
             called = turn - last_seen[prev_called]
         else:
@@ -23,24 +25,30 @@ def part1(input: List[int], turn_goal: int = 2020):
 
         last_seen[prev_called] = turn
         # all_called.append(called)
-        turn += 1
+        # turn += 1
         prev_called = called
-
-        if turn == turn_goal:
-            return (called)
+        yield called
 
 
-test_input = [0, 3, 6]
-assert part1(test_input) == 436
+def get_nth_call(starting_nums, n=2020):
+    game = play_game(starting_nums)
+    for _ in range(n - len(starting_nums) - 1):
+        n = next(game)
 
-test_input = [1, 3, 2]
-assert part1(test_input) == 1
+    return n
 
-input = [19, 20, 14, 0, 9, 1]
-print(part1(input))
 
-test_input = [0, 3, 6]
+test_starting_numing_num = [0, 3, 6]
+assert get_nth_call(test_starting_numing_num) == 436
+
+test_starting_numing_num = [1, 3, 2]
+assert get_nth_call(test_starting_numing_num) == 1
+
+starting_num = [19, 20, 14, 0, 9, 1]
+print(get_nth_call(starting_num))
+
+test_starting_numing_num = [0, 3, 6]
 goal = 30000000
-assert part1(test_input, goal) == 175594
+assert get_nth_call(test_starting_numing_num, goal) == 175594
 
-print(part1(input, goal))
+print(get_nth_call(starting_num, goal))
